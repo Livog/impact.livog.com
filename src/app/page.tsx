a60571cbfc1d230572c8b4f224e2057e4ae154de
Fredrik <livog@live.com>
2025-05-22 23:28:29 +0200

    feat: add header to home page with title "Impact Of UI Components"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="w-80 min-h-60 relative">
+        <h1 className="text-4xl font-bold">Impact Of UI Components</h1>
         <SearchDirect className="absolute top-0 left-0 w-full" />
       </div>
     </div>
