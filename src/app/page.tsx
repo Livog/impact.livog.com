b03517cc1b0e30bc9927b632bcb1e4d2336c18e2
Fredrik <livog@live.com>
2025-05-23 07:37:37 +0200

    fix: update header structure on home page for consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import SearchDirect from "@/components/search-direct";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold text-center">Impact Of UI Components</h1>
       <div className="w-full max-w-screen-sm min-h-60 relative">
-        <h1 className="text-4xl font-bold">Impact Of UI Components</h1>
         <SearchDirect className="absolute top-0 left-0 w-full" />
       </div>
     </div>
