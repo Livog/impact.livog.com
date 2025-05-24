4df4a09f44dfb7ccbf04de6d7bf8a522babe06df
Fredrik <livog@live.com>
2025-05-24 19:57:26 +0200

    fix: update page title from "Shadcn UI" to "Chakra UI"

--- src/app/ui/chakraui/page.tsx
+++ src/app/ui/chakraui/page.tsx
@@ -3,7 +3,7 @@ import { ImpactTable } from '@/components/impact-table'
 export default async function Page() {
   return (
     <div className="mx-auto my-8 max-w-2xl">
-      <h1 className="mb-4 text-2xl font-semibold">Shadcn UI</h1>
+      <h1 className="mb-4 text-2xl font-semibold">Chakra UI</h1>
       <ImpactTable routePattern="/bundles/ui/chakra/:name" trimPrefix={['/bundles/ui/chakra/']} />
     </div>
   )
