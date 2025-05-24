45a514e79c34ec2115a46d284796d090e3bdd136
Fredrik <livog@live.com>
2025-05-24 22:00:56 +0200

    fix: update route pattern for ImpactTable in Chakra UI page

--- src/app/ui/chakraui/page.tsx
+++ src/app/ui/chakraui/page.tsx
@@ -4,7 +4,7 @@ export default async function Page() {
   return (
     <div className="mx-auto my-8 max-w-2xl">
       <h1 className="mb-4 text-2xl font-semibold">Chakra UI</h1>
-      <ImpactTable routePattern="/bundles/ui/chakra/:name" trimPrefix={['/bundles/ui/chakra/']} />
+      <ImpactTable routePattern="/bundles/ui/chakraui/:name" trimPrefix={['/bundles/ui/chakraui/']} />
     </div>
   )
 }
