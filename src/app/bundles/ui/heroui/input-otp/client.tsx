1e4caa82203ae9ba640e5bbdcc6f1e84e095b7ee
Fredrik <livog@live.com>
2025-05-24 09:25:39 +0200

    chore: add headlessui

--- src/app/bundles/ui/heroui/input-otp/client.tsx
+++ src/app/bundles/ui/heroui/input-otp/client.tsx
@@ -6,11 +6,6 @@ import { InputOtp } from '@heroui/react'
 export function InputOtpPage() {
   const [value, setValue] = React.useState('')
   return (
-    <div>
-      <InputOtp length={4} value={value} onValueChange={setValue} />
-      <div className="text-small text-default-500">
-        OTP value: <span className="text-md font-medium">{value}</span>
-      </div>
-    </div>
+    <InputOtp length={4} value={value} onValueChange={setValue} />
   )
 }
