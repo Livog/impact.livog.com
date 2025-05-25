50651dd8b1660734824e77bcb7db2f2391818737
Fredrik <livog@live.com>
2025-05-25 17:28:41 +0200

    Update image source in ImageListClient component

--- src/app/bundles/ui/material/image-list/client.tsx
+++ src/app/bundles/ui/material/image-list/client.tsx
@@ -6,7 +6,7 @@ export default function ImageListClient() {
   return (
     <ImageList>
       <li>
-        <img src="" alt="" />
+        <img src="https://images.unsplash.com/photo-1531746020798-e6953c6e8e04" alt="Image" />
       </li>
     </ImageList>
   )
