60161e0efc0b8a1df20b21f1b1583c9de6c9ffbb
Fredrik <livog@live.com>
2025-05-25 20:13:35 +0200

    Update TransferPage component to include sample data for demonstration

--- src/app/bundles/ui/antd/transfer/client.tsx
+++ src/app/bundles/ui/antd/transfer/client.tsx
@@ -3,5 +3,11 @@
 import { Transfer } from 'antd'
 
 export function TransferPage() {
-  return <Transfer dataSource={[]} targetKeys={[]} render={(item) => item.title} />
+  const dataSource = [
+    { key: '1', title: 'Item 1' },
+    { key: '2', title: 'Item 2' },
+    { key: '3', title: 'Item 3' }
+  ]
+
+  return <Transfer dataSource={dataSource} targetKeys={[]} render={(item) => item.title} />
 }
