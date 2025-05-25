e25b9ad557e08856ccf4e301b34b60e58718a960
Fredrik <livog@live.com>
2025-05-25 17:33:11 +0200

    Update GridClient component to include multiple Grid items with specified sizes

--- src/app/bundles/ui/material/grid/client.tsx
+++ src/app/bundles/ui/material/grid/client.tsx
@@ -4,9 +4,18 @@ import Grid from '@mui/material/Grid'
 
 export default function GridClient() {
   return (
-    <Grid container>
-      <Grid item>
-        <div />
+    <Grid container spacing={2}>
+      <Grid size={8}>
+        <li>size=8</li>
+      </Grid>
+      <Grid size={4}>
+        <li>size=4</li>
+      </Grid>
+      <Grid size={4}>
+        <li>size=4</li>
+      </Grid>
+      <Grid size={8}>
+        <li>size=8</li>
       </Grid>
     </Grid>
   )
